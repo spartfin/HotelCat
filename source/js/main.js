67f30fca5da98d778deeab71f7a3c70c111cbda5
Spartfin <fin.07@bk.ru>
2020-03-17 13:09:05 +0300

    Закончен блок дескопа Отзывы

--- source/js/main.js
+++ source/js/main.js
@@ -3,17 +3,17 @@
 
 (function () {
 
-  /* Slider one*/
+  /* Slider one Блок Номера*/
   var active = 0;
   var prev = 0;
-  var leftArrow = document.querySelector('.numbers__left_arrow');
-  var rightArrow = document.querySelector('.numbers__right_arrow');
+  var numbersLeftArrow = document.querySelector('.numbers__left_arrow');
+  var numbersRightArrow = document.querySelector('.numbers__right_arrow');
   var numbersItem = document.getElementsByClassName('numbers__item');
   var numbersButtonSlide = document.getElementsByClassName('numbers__button_slide');
   var numbersActive = document.querySelectorAll('.numbers__button_slide');
 
   var clickRightArrow = function () {
-    rightArrow.addEventListener('click', function () {
+    numbersRightArrow.addEventListener('click', function () {
       active >= 0 && active < numbersItem.length - 1 ? ++active : active = 0;
       changeActiveSlide();
       buttonActiveSlide();
@@ -22,7 +22,7 @@
   clickRightArrow();
 
   var clickLeftArrow = function () {
-    leftArrow.addEventListener('click', function () {
+    numbersLeftArrow.addEventListener('click', function () {
       active > 0 ? --active : active = numbersItem.length - 1;
       changeActiveSlide();
       buttonActiveSlide();
@@ -45,7 +45,7 @@
     numbersButtonSlide[active].classList.add('numbers__active');
   };
 
-  /* Slider one*/
+  /* Slider two Блок Номера*/
   var activeItem = 0;
   var prevItem = 0;
 
@@ -66,5 +66,65 @@
   var changeActiveItem = function () {
     numbersButtonSlide[prevItem].classList.remove('numbers__active');
     numbersButtonSlide[activeItem].classList.add('numbers__active');
+  };
+
+  /* Slider one Блок Номера*/
+  var reviewsLeftArrow = document.querySelector('.reviews__left_arrow');
+  var reviewsRightArrow = document.querySelector('.reviews__right_arrow');
+  var reviewsItem = document.getElementsByClassName('reviews__item');
+  var reviewsButtonSlide = document.getElementsByClassName('reviews__button_slide');
+  var reviewsActive = document.querySelectorAll('.reviews__button_slide');
+
+  var reviewsClickRightArrow = function () {
+    reviewsRightArrow.addEventListener('click', function () {
+      active >= 0 && active < reviewsItem.length - 1 ? ++active : active = 0;
+      reviewsChangeActiveSlide();
+      reviewsButtonActiveSlide();
+    });
+  };
+  reviewsClickRightArrow();
+
+  var reviewsClickLeftArrow = function () {
+    reviewsLeftArrow.addEventListener('click', function () {
+      active > 0 ? --active : active = reviewsItem.length - 1;
+      reviewsChangeActiveSlide();
+      reviewsButtonActiveSlide();
+    });
+  };
+  reviewsClickLeftArrow();
+
+  var reviewsChangeActiveSlide = function () {
+    reviewsItem[prev].classList.remove('reviews__item_active');
+    reviewsItem[active].classList.add('reviews__item_active');
+    for (var q = 0; q < reviewsItem.length; q++) {
+      reviewsItem[q].style.transform = 'translate(' + -(active * 100) + '%)';
+    }
+  };
+
+  var reviewsButtonActiveSlide = function () {
+    for (var q = 0; q < reviewsActive.length; q++) {
+      reviewsActive[q].classList.remove('reviews__active');
+    }
+    reviewsButtonSlide[active].classList.add('reviews__active');
+  };
+
+  /* Slider two Блок Номера*/
+  for (var q = 0; q < reviewsButtonSlide.length; q++) {
+    // eslint-disable-next-line no-shadow
+    var reviewsfuncButton = function (q) {
+      reviewsButtonSlide[q].addEventListener('click', function () {
+        prevItem = activeItem;
+        activeItem = q;
+        active = activeItem;
+        reviewsChangeActiveItem();
+        reviewsChangeActiveSlide();
+      });
+    };
+    reviewsfuncButton(q);
+  }
+
+  var reviewsChangeActiveItem = function () {
+    reviewsButtonSlide[prevItem].classList.remove('reviews__active');
+    reviewsButtonSlide[activeItem].classList.add('reviews__active');
   };
 })();
